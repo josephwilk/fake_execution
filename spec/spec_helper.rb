135c5b4beab14893d75ce7f735800e2411322a92
Joseph Wilk <joe@josephwilk.net>
2011-12-09 18:03:44 +0000

    Switch to using the rspec way of running fake_execution

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 
-require 'fake_execution'
+require 'fake_execution/spec_helpers'
+
+RSpec.configure do |config|
+  config.extend FakeExecution::SpecHelpers
+end
